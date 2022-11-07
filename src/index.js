2f803615f9021dbb7700d3be29a86341f3e5685c
AdhiamboOuma <adhiamboconnie@gmail.com>
2022-11-07 11:57:04 +0300

    add first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+require("./db/mongoose");
+const User = require("./models/user");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/users", (req, res) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then(() => {
+      res.send(user);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+app.get('/users', (req, res) => {
+  User.find ({})
+  .then((users) => {
+      res.json(users);
+  }) 
+  .catch ((error) => { 
+  res.status(500).send(error);
+});
+});
+
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
+});
